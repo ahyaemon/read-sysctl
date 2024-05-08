d5c427c2d9f96ad1f33b6da0a5ae5605e8ff7ca0
ahyaemon <iamahyaemon@gmail.com>
2024-05-09 07:41:18 +0900

    ハッシュマップを作成

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::{env, process};
+use std::collections::HashMap;
+use std::io::Error;
 use crate::config::Config;
+use crate::file::read_lines;
+use crate::parse::parse_line;
 
 mod config;
 mod file;
@@ -7,9 +11,50 @@ mod parse;
 
 fn main() {
     let args = env::args();
-    let config = Config::new(args).unwrap_or_else(|err| {
-        println!("コマンドライン引数の取得に失敗しました: {}", err);
-        process::exit(1);
-    });
-    println!("{:?}", config.filename);
+    let config = Config::new(args)
+        .unwrap_or_else(|err| {
+            println!("コマンドライン引数の取得に失敗しました: {}", err);
+            process::exit(1);
+        });
+    let result = create_hashmap(&config.filename)
+        .unwrap_or_else(|err| {
+            println!("ハッシュマップの作成に失敗しました: {}, filename: {}", err, &config.filename);
+            process::exit(1);
+        });
+    println!("{:?}", result);
+}
+
+fn create_hashmap(filename: &str) -> Result<HashMap<String, String>, Error> {
+    let hashmap = read_lines(filename)?
+        .flatten()
+        .filter_map(|line| parse_line(&line) )
+        .collect();
+    Ok(hashmap)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create() {
+        let filename = "resources/sysctl.conf";
+        let actual = create_hashmap(&filename).unwrap();
+        let expected = vec![
+            ("debug", "true"),
+            ("endpoint", "localhost:3000"),
+            ("log.file", "/var/log/console.log")
+        ]
+            .iter()
+            .map(|(key, value)| (key.to_string(), value.to_string()))
+            .collect::<HashMap<_, _>>();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    #[should_panic]
+    fn file_not_exists() {
+        let filename = "resources/xxx";
+        create_hashmap(&filename).unwrap();
+    }
 }
