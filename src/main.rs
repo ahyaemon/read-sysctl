1fe31664b25c8f8403cd0310f5948d5a9f50fdab
ahyaemon <iamahyaemon@gmail.com>
2024-05-12 22:13:15 +0900

    hashmap 作成関数をファイル分け

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::{env, process};
 use std::collections::HashMap;
-use std::hash::Hash;
 use crate::config::Config;
 use crate::file::read_lines;
 use crate::parse::parse_line;
+use crate::sysctl::read_sysctl;
 
 mod config;
 mod file;
 mod parse;
+mod sysctl;
 
 type Dict = HashMap<String, String>;
 
@@ -31,7 +32,7 @@ fn main() {
 
     println!("{:?}", schema.unwrap());
 
-    let result = create_hashmap(&config.filename)
+    let result = read_sysctl(&config.filename)
         .unwrap_or_else(|err| {
             println!("ハッシュマップの作成に失敗しました: {} filename: {}", err, &config.filename);
             process::exit(1);
@@ -48,86 +49,4 @@ fn create_schema(filename: &str) -> Result<Dict, String> {
         }
     }
     Ok(hashmap)
-}
-
-fn create_hashmap(filename: &str) -> Result<Dict, String> {
-    let lines = read_lines(filename).map_err(|e| e.to_string())?;
-    let mut hashmap = HashMap::new();
-    for line in lines.flatten() {
-        if let Some((key, value)) = parse_line(&line, "=")? {
-            hashmap.insert(key, value);
-        }
-    }
-    Ok(hashmap)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn create() {
-        let filename = "resources/sysctl.conf";
-        let actual = create_hashmap(&filename);
-        let expected = Ok(
-            [
-                ("debug", "true"),
-                ("endpoint", "localhost:3000"),
-                ("log.file", "/var/log/console.log")
-            ]
-                .iter()
-                .map(|(key, value)| (key.to_string(), value.to_string()))
-                .collect()
-        );
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn duplicate() {
-        let filename = "resources/sysctl_duplicate.conf";
-        let actual = create_hashmap(&filename);
-        let expected = Ok(
-            [
-                ("key".to_string(), "value2".to_string()),
-            ]
-                .iter()
-                .cloned()
-                .collect()
-        );
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn file_not_exists() {
-        let filename = "resources/xxx";
-        match create_hashmap(&filename) {
-            Ok(_) => { panic!() },
-            Err(e) => { println!("{e}") }
-        }
-    }
-
-    #[test]
-    fn invalid_line() {
-        let filename = "resources/sysctl_invalid.conf";
-        match create_hashmap(&filename) {
-            Ok(_) => { panic!() },
-            Err(e) => { println!("{e}") }
-        }
-    }
-
-    #[test]
-    fn invalid_line_with_hyphen() {
-        let filename = "resources/sysctl_invalid_hyphen.conf";
-        let actual = create_hashmap(&filename);
-        let expected = Ok(
-            vec![
-                ("key1", "value1"),
-                ("key2", "value2"),
-            ]
-                .iter()
-                .map(|(key, value)| (key.to_string(), value.to_string()))
-                .collect()
-        );
-        assert_eq!(actual, expected);
-    }
 }
---
+++ src/sysctl.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+use crate::{Dict};
+use crate::file::read_lines;
+use crate::parse::parse_line;
+
+pub fn read_sysctl(filename: &str) -> Result<Dict, String> {
+    let lines = read_lines(filename).map_err(|e| e.to_string())?;
+    let mut hashmap = HashMap::new();
+    for line in lines.flatten() {
+        if let Some((key, value)) = parse_line(&line, "=")? {
+            hashmap.insert(key, value);
+        }
+    }
+    Ok(hashmap)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create() {
+        let filename = "resources/sysctl.conf";
+        let actual = read_sysctl(&filename);
+        let expected = Ok(
+            [
+                ("debug", "true"),
+                ("endpoint", "localhost:3000"),
+                ("log.file", "/var/log/console.log")
+            ]
+                .iter()
+                .map(|(key, value)| (key.to_string(), value.to_string()))
+                .collect()
+        );
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn duplicate() {
+        let filename = "resources/sysctl_duplicate.conf";
+        let actual = read_sysctl(&filename);
+        let expected = Ok(
+            [
+                ("key".to_string(), "value2".to_string()),
+            ]
+                .iter()
+                .cloned()
+                .collect()
+        );
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn file_not_exists() {
+        let filename = "resources/xxx";
+        match read_sysctl(&filename) {
+            Ok(_) => { panic!() },
+            Err(e) => { println!("{e}") }
+        }
+    }
+
+    #[test]
+    fn invalid_line() {
+        let filename = "resources/sysctl_invalid.conf";
+        match read_sysctl(&filename) {
+            Ok(_) => { panic!() },
+            Err(e) => { println!("{e}") }
+        }
+    }
+
+    #[test]
+    fn invalid_line_with_hyphen() {
+        let filename = "resources/sysctl_invalid_hyphen.conf";
+        let actual = read_sysctl(&filename);
+        let expected = Ok(
+            vec![
+                ("key1", "value1"),
+                ("key2", "value2"),
+            ]
+                .iter()
+                .map(|(key, value)| (key.to_string(), value.to_string()))
+                .collect()
+        );
+        assert_eq!(actual, expected);
+    }
+}
